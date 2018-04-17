bd4def06e01fe126384d0071d058a09c689785c2
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 18:41:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,18 +14,14 @@ class Dog
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE dogs
-    SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute("DROP TABLE dogs")
   end
 
   def save
@@ -44,58 +40,25 @@ class Dog
   end
 
   def update
-    sql = <<-SQL
-      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
-    SQL
-
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.new_from_db(row)
-    self.new(id: row[0], name: row[1], breed: row[2])
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE dogs.name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-  def self.find_by_id(id)
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE id = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, id).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
   def self.create(attributes)
-    new_dog = self.new(attributes)
-    new_dog.save
-  end
-
-  def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE dogs.name = ? AND dogs.breed = ?", name, breed)
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-    else
-      dog = self.create(name: name, breed: breed)
-    end
+    dog = Dog.new(attributes)
+    dog.save
     dog
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+    result = DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.new_from_db
+    
+  end
 
 end
